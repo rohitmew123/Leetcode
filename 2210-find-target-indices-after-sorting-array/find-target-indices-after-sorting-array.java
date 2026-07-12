5755883f4382f653509156882ebab34db4a05cfb
Rohit <mewadarohit048@gmail.com>
2026-07-12 22:22:00 +0530

    Time: 5 ms (72.81%) | Memory: 44.8 MB (76.33%) - LeetSync

---
+++ 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<Integer> targetIndices(int[] nums, int target) {
+        Arrays.sort(nums);
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for(int i=0; i<nums.length; i++) {
+            if(target == nums[i]) {
+                list.add(i);
+
+            }
+        }
+        return list;
+
+
+        
+    }
+}
